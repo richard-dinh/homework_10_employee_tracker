e5d1fa2ce41024f92ba432bbc39d88ab627342f0
richard-dinh <richarddinh@gmail.com>
2020-02-24 18:24:01 -0800

    began writing seed data

---
+++ db/seed.sql
@@ -0,0 +1,18 @@
+-- Department Seed Data
+USE employees_db;
+
+INSERT INTO departments(name)
+VALUES
+  ('Engineering'),
+  ('Human Resources'),
+  ('Finance'),
+  ('Sales'),
+  ('Marketing'),
+  ('Construction')
+
+-- Role Seed Data
+USE employees_db;
+
+INSERT INTO roles(title, salary)
+
+-- Employee Seed Data
