c610827beaba1580fdf8946af1064ec96233bd50
pinheadrarry <richarddinh@gmail.com>
2020-02-25 14:29:39 -0800

    updated schema

--- db/schema.sql
+++ db/schema.sql
@@ -12,7 +12,7 @@ CREATE TABLE departments(
 CREATE TABLE roles(
   id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
   title VARCHAR(30) NOT NULL,
-  salary decimal NOT NULL,
+  salary DECIMAL NOT NULL,
   department_id INT,
   FOREIGN KEY (department_id) REFERENCES departments(id)
 );
@@ -21,7 +21,7 @@ CREATE TABLE employees(
   id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id INT,
+  role_id INT NOT NULL,
   manager_id INT REFERENCES employees(id),
   FOREIGN KEY (role_id) REFERENCES roles(id)
 );
