fee718a3dff861821dbf3f1a7a3150564091d4f5
richard-dinh <richarddinh@gmail.com>
2020-02-25 17:51:46 -0800

    wrote route to post a new employee

--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -3,6 +3,7 @@ const db = require('../config/db.js')
 
 //GET ROUTES
 
+//Employee Get Routes
 //Get all employees
 router.get('/employees', (request, response)=>{
   db.query('SELECT * FROM employees', (error, employees)=>{
@@ -10,9 +11,45 @@ router.get('/employees', (request, response)=>{
     response.json(employees)
   })
 })
+
+
+//Roles Get Routes
 //Get all roles
+router.get('/roles', (request, response)=>{
+  db.query('SELECT * FROM roles', (error, roles)=>{
+    if(error){console.error(error)}
+    response.json(roles)
+  })
+})
 
+//Department Get Routes
 //Get all departments
+router.get('/departments', (request, response)=>{
+  db.query('SELECT * FROM departments', (error, departments)=>{
+    if(error){console.error(error)}
+    response.json(departments)
+  })
+})
 
+//POST ROUTES
+//Employee Post Routes
+//Create a new Employee
+router.post('/employees', (request, response)=>{
+  // request.body must come in Object format:
+  /*{
+    id,
+    first_name,
+    last_name,
+    role_id,
+    manager_id (optional)
+  }*/
+  db.query('INSERT INTO employees SET ?', request.body, error=>{
+    if(error){console.error(error)}
+    response.sendStatus(200)
+  })
+})
 
+//Roles Post Routes
+
+//Department Post Routes
 module.exports = router
