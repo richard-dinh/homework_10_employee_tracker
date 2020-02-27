34aad0992ee7c94bb3bd1810b564c2494c261e92
richard-dinh <richarddinh@gmail.com>
2020-02-26 19:52:40 -0800

    set up app.js with all modules

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-const express = require('express')
+//Bring in routes
+const { getEmployees, getEmployee, createEmployee, updateEmployee, deleteEmployee } = require('./controllers/employeeController.js')
+const { getDepartments, getDepartment, createDepartment, updateDepartment, deleteDepartment } = require('./controllers/departmentController.js')
+const { getRoles, getRole, createRole, updateRole, deleteRole } = require('./controllers/roleController.js')
 
-const app = express()
+// bring in console.table
+const cTable = require('console.table')
+//bring in inqurier
+const prompt = require('inquirer').createPromptModule()
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
 
-// ues index.js in the routes folder and brings all routes from that file
-app.use(require('./routes'))
+
