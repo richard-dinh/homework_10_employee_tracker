18f8d943c076d8664857113449a90d515aba2283
richard-dinh <richarddinh@gmail.com>
2020-02-26 20:13:49 -0800

    made inquirer to prompt for user choice

--- app.js
+++ app.js
@@ -8,16 +8,49 @@ const cTable = require('console.table')
 //bring in inqurier
 const prompt = require('inquirer').createPromptModule()
 
-// const init = async () =>{
-//   let {choice} = prompt([
-//     {
-//       type: 'rawlist',
-//       name: 'choice',
-//       message: 'What would you like to do?',
-//       choices: ['View All Employees by Department', 'View all Employees by Manager, Add Employee, Remove Employee, Update Employee Role, Update Employee Manager, View All Roles']
-//     }
-//   ])
-// }
+const init = async () => {
+  let { choice } = await prompt([
+    {
+      type: 'list',
+      name: 'choice',
+      message: 'What would you like to do?',
+      choices: ['View All Employees by Department', 'View all Employees by Manager', 'Add Employee', 'Remove Employee', 'Update Employee Role', 'Update Employee Manager', 'View All Roles']
+    }
+  ])
+  switch (choice) {
+    case 'View All Employees by Department':
+      console.log('dept')
+      init()
+      break
+    case 'View all Employees by Manager':
+      console.log('manager')
+      init()
+      break
+    case 'Add Employee':
+      console.log('Add')
+      init()
+      break
+    case 'Remove Employee':
+      console.log('remove')
+      init()
+      break
+    case 'Update Employee Role':
+      console.log('update role')
+      init()
+      break
+    case 'Update Employee Manager':
+      console.log('manager')
+      init()
+      break
+    case 'View All Roles':
+      console.log('view all roles')
+      init()
+      break
+    case 'EXIT':
+      process.exit()
+      break
+  }
+}
 
-// init()
+init()
 
