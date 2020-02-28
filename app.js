b94fe7f04938ec314304f519075abbffab37a47b
richard-dinh <richarddinh@gmail.com>
2020-02-27 18:11:44 -0800

    finished logic for getting employees by dept

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const prompt = require('inquirer').createPromptModule()
 
 
 //replaces the manager id with the employee's name
-const replaceManagerID = (names, employees)=>{
+const replaceManagerID = (names, employees) => {
   for (let i = 0; i < employees.length; i++) {
     for (let j = 0; j < names.length; j++) {
       if (employees[i].manager_id === names[j].employee_id) {
@@ -26,6 +26,18 @@ const replaceManagerID = (names, employees)=>{
 }
 
 const init = async () => {
+  //populating names array with employee id and combined first and last name
+  let names = []
+  getEmployeesWithID(data => {
+    data.forEach(person => {
+      names.push(
+        {
+          employee_id: person.employee_id,
+          name: `${person.first_name} ${person.last_name}`
+        }
+      )
+    })
+  })
   let { choice } = await prompt([
     {
       type: 'list',
@@ -36,30 +48,37 @@ const init = async () => {
   ])
   switch (choice) {
     case 'View all Employees':
-        getEmployees(employees =>{
-          employees = replaceManagerID(names, employees)
-          console.table(employees)
-          init()
-        })
-      break
-    case 'View all Employees by Department':
-      getDepartments(departments =>{
-        let department_names =[]
-        //populating department_names with the choices for prompt
-        departments.forEach(department =>{
-          department_names.push(department.department_name)
-        })
-        console.log(department_names)
+      getEmployees(employees => {
+        employees = replaceManagerID(names, employees)
+        console.table(employees)
         init()
       })
-      // let {deptName} = prompt([
-      //   {
-      //     type: 'list',
-      //     name: deptName,
-      //     message: "Which Department would you like to view?",
-      //     choices:
-      //   }
-      // ])
+      break
+    case 'View all Employees by Department':
+      getDepartments(departments => {
+        let department_names = []
+        //populating department_names with the choices for prompt
+        departments.forEach(department => {
+          department_names.push(department.department_name)
+        })
+        //prompt the user with the deptartment names as choices
+        prompt([
+          {
+            type: 'list',
+            name: 'deptName',
+            message: "Which Department would you like to view?",
+            choices: department_names
+          }
+        ])
+        .then(({deptName})=>{
+          //run query to get all employees in a deptarment
+          getEmployeesByDepartment(deptName, dept =>{
+            console.table(dept)
+            init()
+          })
+        })
+        .catch(error =>console.error(error))
+      })
       break
     case 'View all Employees by Manager':
       console.log('manager')
@@ -84,16 +103,5 @@ const init = async () => {
   }
 }
 
-//populating names array with employee id and combined first and last name
-let names = []
-getEmployeesWithID(data => {
-  data.forEach(person => {
-    names.push(
-      {
-      employee_id: person.employee_id,
-      name: `${person.first_name} ${person.last_name}`
-      }
-    )
-  })
-})
+
 init()
